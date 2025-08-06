4a0f726f91d43f017e99952b3c52bec7f7220c98
a0925083098 <a0925083098@gmail.com>
2025-08-06 15:56:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! Your bot is working!")
+    await update.message.reply_text("你好，我是機器人！")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
